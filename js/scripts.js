c3a446cfae384ffe407bef48116707830800fce4
Jacek Syguła <sygulajack@gmail.co>
2024-02-11 21:08:23 +0100

    add script js

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const btn = document.querySelector("button");
+let counter = 0;
+
+btn.addEventListener("click", () => {
+   
+    const h1 = document.querySelector("h1");
+
+
+    if (counter ==0) {
+        h1.innerText = "Zapraszamy do nas!";
+        counter++;
+    } else {
+        h1.innerText = "Obroni";
+        counter--;
+    }
+
+
+});
